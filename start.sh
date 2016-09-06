23cad8201d83a7c943e0c520d72e0d8aef78e8d9
Bryan <Bryan@resin.io>
2016-09-06 12:27:12 -0700

    m

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ if [ -z "$ROTATE" ]; then
     ROTATE=0
 fi
 
-convert images/docker.png -rotate $ROTATE images/rotated.png
+convert images/resin.png -rotate $ROTATE images/rotated.png
 fbi -d /dev/fb1 -T 1 -noverbose -a images/rotated.png
 
 while true; do
