f608ea761bba2d4177961ded49b7ca696dde82ec
Gergely Imreh <imrehg@gmail.com>
2016-12-01 10:57:07 +0000

    display image from URL

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,9 @@
 FROM resin/rpi-raspbian:jessie-20160511
 
+WORKDIR /usr/src/app
+
 RUN apt-get update && apt-get -y install fbi imagemagick
 
 COPY . /usr/src/app
-WORKDIR /usr/src/app
 
 CMD ./prestart.sh && ./start.sh
--- start.sh
+++ start.sh
@@ -6,6 +6,12 @@ if [ -z "$ROTATE" ]; then
     ROTATE=0
 fi
 
+if [ -n "$URL" ]; then
+  echo "Downloading: ${URL}"
+  wget "${URL}" images/web-image.jpg
+  IMAGE="web-image.jpg"
+fi
+
 convert images/${IMAGE-image.png} -rotate $ROTATE images/rotated.png
 fbi -d /dev/fb1 -T 1 -noverbose -a images/rotated.png
 
